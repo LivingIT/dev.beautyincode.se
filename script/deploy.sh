fff0a6f5b7605b41f6c5592aa529640f6bd2e10e
Fredrik Frodlund <fredrik@frodlund.se>
2020-01-10 20:04:19 +0100

    Add deploy scripts

--- script/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-pwd
-yarn run deploy
-yarn run jekyll:build
-cd _site
-git add --all
-git commit -m "Deploy web site at $(date -u)"
-git push
---
+++ scripts/deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+yarn install --production --pure-lockfile
+# yarn deploy
+jekyll build --config "_config.yml,_config.prod.yml"
