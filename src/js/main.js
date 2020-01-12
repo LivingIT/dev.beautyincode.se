4476cf36707afa85b9e0a9b7bbb84a0c53e7491f
frippz <frippz@users.noreply.github.com>
2020-01-12 15:46:41 +0000

    Automated deployment: Sun Jan 12 15:46:41 UTC 2020 3f37c8414487ba44f80b2f103506f86c246e6c39

--- src/js/main.js
+++ src/js/main.js
@@ -12,59 +12,59 @@ var didCutTheMustard = 'querySelector' in document
   && 'classList' in document.createElement('a')
   && 'addEventListener' in window;
 
+/**
+ * Countdown timer
+ */
+function countdown(endDate) {
+  const countDownElem = document.querySelector('#countdown');
+
+  if (!countDownElem) return;
+
+  let days, hours, minutes, seconds;
+
+  endDate = new Date(endDate).getTime();
+
+  if (isNaN(endDate)) {
+    return;
+  }
+
+  setInterval(calculate, 1000);
+
+  function calculate() {
+    let startDate = new Date().getTime();
+
+    let timeRemaining = parseInt((endDate - startDate) / 1000);
+
+    if (timeRemaining >= 0) {
+      days = parseInt(timeRemaining / 86400);
+      timeRemaining = (timeRemaining % 86400);
+
+      hours = parseInt(timeRemaining / 3600);
+      timeRemaining = (timeRemaining % 3600);
+
+      minutes = parseInt(timeRemaining / 60);
+      timeRemaining = (timeRemaining % 60);
+
+      seconds = parseInt(timeRemaining);
+
+      countDownElem.querySelector('.days').innerHTML = parseInt(days, 10);
+      countDownElem.querySelector('.hours').innerHTML = hours < 10 ? "0" + hours : hours;
+      countDownElem.querySelector('.minutes').innerHTML = minutes < 10 ? "0" + minutes : minutes;
+      countDownElem.querySelector('.seconds').innerHTML = seconds < 10 ? "0" + seconds : seconds;
+    } else {
+      return;
+    }
+
+    const isActive = countDownElem.classList.contains('active');
+
+    if (!isActive) countDownElem.classList.add('active');
+  }
+}
+
 if (didCutTheMustard) {
 
   // Add class "js" to html element
   document.querySelector('html').classList.add('js');
-
-  /**
-   * Countdown timer
-   */
-  function countdown(endDate) {
-    const countDownElem = document.querySelector('#countdown');
-
-    if (!countDownElem) return;
-
-    let days, hours, minutes, seconds;
-
-    endDate = new Date(endDate).getTime();
-
-    if (isNaN(endDate)) {
-      return;
-    }
-
-    setInterval(calculate, 1000);
-
-    function calculate() {
-      let startDate = new Date().getTime();
-
-      let timeRemaining = parseInt((endDate - startDate) / 1000);
-
-      if (timeRemaining >= 0) {
-        days = parseInt(timeRemaining / 86400);
-        timeRemaining = (timeRemaining % 86400);
-
-        hours = parseInt(timeRemaining / 3600);
-        timeRemaining = (timeRemaining % 3600);
-
-        minutes = parseInt(timeRemaining / 60);
-        timeRemaining = (timeRemaining % 60);
-
-        seconds = parseInt(timeRemaining);
-
-        countDownElem.querySelector('.days').innerHTML = parseInt(days, 10);
-        countDownElem.querySelector('.hours').innerHTML = hours < 10 ? "0" + hours : hours;
-        countDownElem.querySelector('.minutes').innerHTML = minutes < 10 ? "0" + minutes : minutes;
-        countDownElem.querySelector('.seconds').innerHTML = seconds < 10 ? "0" + seconds : seconds;
-      } else {
-        return;
-      }
-
-      const isActive = countDownElem.classList.contains('active');
-
-      if (!isActive) countDownElem.classList.add('active');
-    }
-  }
 
   // Start the countdown
   countdown('2020/03/07 09:00:00');
