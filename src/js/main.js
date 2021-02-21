2b3446bb3d3c0c36eb0077f8ae247f507b6e53c9
frippz <frippz@users.noreply.github.com>
2021-02-21 10:08:23 +0000

    Automated deployment: Sun Feb 21 10:08:23 UTC 2021 LivingIT/beautyincode.se@3dd951c69210c5ff2a7e8d856e4dbcce7d52632f

--- src/js/main.js
+++ src/js/main.js
@@ -67,7 +67,7 @@ if (didCutTheMustard) {
   document.querySelector('html').classList.add('js');
 
   // Start the countdown
-  countdown('2021/03/06 09:00:00');
+  countdown('2022/03/05 09:00:00');
 }
 
 function scrollToTop() {
